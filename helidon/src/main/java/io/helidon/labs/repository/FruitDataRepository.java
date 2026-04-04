ffbeb5591d05a8ade7bc4b85c7bca9c4d8f2b191
hyder <ali.mukadam@oracle.com>
2026-04-04 11:54:46 +1100

    fix: switch back to string concatenation
    Signed-off-by: hyder <ali.mukadam@oracle.com>

--- helidon/src/main/java/io/helidon/labs/repository/FruitDataRepository.java
+++ helidon/src/main/java/io/helidon/labs/repository/FruitDataRepository.java
@@ -8,28 +8,23 @@ import java.util.Optional;
 
 @Data.Repository
 public interface FruitDataRepository extends Data.CrudRepository<Fruit, Long> {
-  @Data.Query("""
-    SELECT new io.helidon.labs.dto.FruitListRow(
-      f.id, f.name, f.description,
-      s.id, s.name, s.currency,
-      s.address.address, s.address.city, s.address.country,
-      sp.price
-    )
-    FROM Fruit f
-    LEFT JOIN f.storePrices sp
-    LEFT JOIN sp.store s
-    ORDER BY f.name, s.name
-    """)
+  @Data.Query(
+    "SELECT new io.helidon.labs.dto.FruitListRow(" +
+      "f.id, f.name, f.description, " +
+      "s.id, s.name, s.currency, " +
+      "s.address.address, s.address.city, s.address.country, " +
+      "sp.price) " +
+      "FROM Fruit f " +
+      "LEFT JOIN f.storePrices sp " +
+      "LEFT JOIN sp.store s " +
+      "ORDER BY f.name, s.name"
+  )
   List<FruitListRow> listSummaryOrderByName();
 
-  @Data.Query("""
-    SELECT DISTINCT f
-    FROM Fruit f
-    LEFT JOIN FETCH f.storePrices sp
-    LEFT JOIN FETCH sp.store s
-    WHERE f.name = :name
-    ORDER BY s.name
-    """)
+  @Data.Query(
+    "SELECT DISTINCT f FROM Fruit f LEFT JOIN FETCH f.storePrices sp LEFT JOIN FETCH sp.store s " +
+      "WHERE f.name = :name ORDER BY s.name"
+  )
   Optional<Fruit> findDetailedByName(String name);
 
   Optional<Fruit> findByName(String name);
